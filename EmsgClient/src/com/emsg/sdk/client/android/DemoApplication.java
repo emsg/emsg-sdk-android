51339564f935688cd87edb5ed8f275b229fe45fc
zhujohnle <545948101@qq.com>
2014-12-01 12:07:26 +0800

    init import

---
+++ EmsgClient/src/com/emsg/sdk/client/android/DemoApplication.java
@@ -0,0 +1,44 @@
+
+package com.emsg.sdk.client.android;
+
+import android.app.Application;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.emsg.sdk.EmsgClient;
+import com.emsg.sdk.EmsgClient.EmsStateCallBack;
+
+public class DemoApplication extends Application {
+    private static final String TAG = Application.class.getSimpleName();
+
+    private EmsgClient mEmsgClient = null;
+
+    public EmsgClient getEmsgClient() {
+        return mEmsgClient;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mEmsgClient = EmsgClient.getInstance();
+        mEmsgClient.init(this);
+        mEmsgClient.setEmsStCallBack(new EmsStateCallBack() {
+          
+            @Override
+            public void onAnotherClientLogin() {
+                Toast.makeText(DemoApplication.this, "被迫下线", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public void onTerminate() {
+        try {
+            mEmsgClient.closeClient();
+        } catch (Exception ex) {
+            Log.e(TAG, "尝试关闭client异常." + ex.getMessage(), ex);
+        }
+        super.onTerminate();
+    }
+
+}
